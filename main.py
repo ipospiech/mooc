620b9bedf7d96ad08ecf82452928ef49d771662f
ipospiech <147543751+ipospiech@users.noreply.github.com>
2023-10-15 11:24:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(user_name):
+def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
 
-    print(f'Hiya, {user_name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 
